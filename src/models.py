242e1ca7188c7165fd34e9bf9f35b4739776b17a
jhcamara <jhcamara@gmail.com>
2022-11-16 20:31:27 +0000

    uploading files

--- src/models.py
+++ src/models.py
@@ -8,23 +8,82 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    population = Column(Integer)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(Integer)
+    name = Column(String(250))
+    url = Column(String(250))
+
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250))
+    starship_class = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(Integer)
+    length = Column(Integer)
+    crew = Column(Integer)
+    passengers = Column(Integer)
+    max_atmosphering_speed = Column(Integer)
+    hyperdrive_rating = Column(Integer)
+    MGLT = Column(Integer)
+    cargo_capacity = Column(Integer)
+    consumables = Column(String(250))
+    pilots = Column(Integer)
+    name = Column(String(250))
+    url = Column(String(250))
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    height = Column(Integer)
+    mass = Column(Integer)
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+    birth_year = Column(Integer)
+    gender = Column(String(50))
+    name = Column(String(50))
+    homeworld = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship(Planets)
+    url = Column(String(500))
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    favId = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(Characters)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicles = relationship(Vehicles)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship(Planets)    
+
+
+
+    
 
     def to_dict(self):
         return {}
